dbcf29f086b07f8a1f2da0b877158b4e5a6b3baf
felipe_teodoro <felipe@firstcontrol.com.br>
2020-08-13 00:56:01 -0300

    adiciona permissão em scope

--- Auth/defines.h
+++ Auth/defines.h
@@ -9,7 +9,7 @@ constexpr const char *UrlAutorization = "https://accounts.spotify.com/authorize"
 
 constexpr const char *UrlToken = "https://accounts.spotify.com/api/token";
 
-constexpr const char *scope = "user-read-private user-top-read playlist-read-private playlist-modify-public playlist-modify-private";
+constexpr const char *scope = "user-read-private user-top-read playlist-read-private playlist-modify-public playlist-modify-private user-modify-playback-state";
 
 namespace Structs
 {
